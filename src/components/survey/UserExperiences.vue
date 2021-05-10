c3f98c530c3e16b78fc7e9e4a424e8228e4b939d
kmorgport <kmorgan0057@gmail.com>
2021-05-10 17:33:11 -0500

    http request practice wit vue

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -3,7 +3,7 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
       <ul>
         <survey-result
@@ -21,10 +21,30 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
+  // props: ['results'],
   components: {
     SurveyResult,
   },
+  data(){
+    return{
+      results:[]
+    }
+  },
+  methods:{
+    loadExperiences(){
+      fetch('https://vue-http-demo-8bcc6-default-rtdb.firebaseio.com/surveys.json').then(response =>{
+        if(response.ok){
+          return response.json();
+        }
+      }).then(data=>{
+        const apiResults = [];
+        for(const id in data){
+          apiResults.push({id:id, name: data[id].name, rating: data[id].rating})
+        }
+        this.results = apiResults;
+      })
+    }
+  }
 };
 </script>
 

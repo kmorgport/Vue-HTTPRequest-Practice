818517ec3582d29d781f93da33803e86c101bb8b
kmorgport <kmorgan0057@gmail.com>
2021-05-10 23:30:57 -0500

    error catching in vue

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -5,7 +5,9 @@
       <div>
         <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
-      <ul>
+      <p v-if="isLoading">Loading</p>
+      <p v-else-if="!isLoading&&error">{{ error }}</p>
+      <ul v-else-if="!isLoading && results.length>0 ">
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -27,21 +29,29 @@ export default {
   },
   data(){
     return{
-      results:[]
-    }
+      results:[],
+      isLoading: false,
+      error: null
+    };
   },
   methods:{
     loadExperiences(){
+      this.isLoading = true;
       fetch('https://vue-http-demo-8bcc6-default-rtdb.firebaseio.com/surveys.json').then(response =>{
         if(response.ok){
           return response.json();
         }
       }).then(data=>{
+        this.isLoading = false;
         const apiResults = [];
         for(const id in data){
           apiResults.push({id:id, name: data[id].name, rating: data[id].rating})
         }
         this.results = apiResults;
+      })
+      .catch((error)=>{
+        this.isLoading = false;
+        this.error = "Failed to fetch data - please try again"
       })
     }
   },
